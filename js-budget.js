524fdba5bf41ea00e7fa58c40e821ca7901071a0
christinelant <christinelantigua@yahoo.com>
2021-05-03 23:53:01 -0400

    initial commit

---
+++ js-budget.js
@@ -0,0 +1,57 @@
+const addExpense = document.querySelector(".add")
+const removeExpense = document.querySelector(".remove")
+const list = document.querySelector(".entered-expenses")
+const form = document.querySelector('form')
+
+form.addEventListener('submit', function (event) {
+    event.preventDefault()
+    addNewExpense()
+})
+
+const expenses = []
+
+const addNewExpense = () => {
+    // this will grab the values from the input fields and create an object, which will push into expenses
+
+    const expenseName = document.querySelector(".name");
+    const expenseValue = document.querySelector(".amount");
+
+    const expense = {
+        name: expenseName.value,
+        amount: parseFloat(expenseValue.value)
+    }
+
+    expenses.push(expense)
+
+    // clear input values
+    expenseName.value = ""
+    expenseValue.value = ""
+
+    // call buildExpenses
+    buildExpenses()
+}
+
+
+const buildExpenses = () => {
+    // function loops through the expenses array and builds out the innerHTML and then set it into the expense HTML list
+    let str = ""
+
+    for (i = 0; i < expenses.length; i++) {
+        console.log(expenses[i])
+        str += `
+        <li>
+            ${expenses[i].name} ${expenses[i].amount}
+            <button class='remove'>-</button>
+        </li>
+        `
+    }
+
+    list.innerHTML = str
+}
+
+// removes an expense from the list
+removeExpense.addEventListener("click", function (event) {
+    event.preventDefault();
+    list.removeChild(list.lastElementChild);
+});
+
